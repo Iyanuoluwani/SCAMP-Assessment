6e5979acd7ae0f260c703a3f629a68cbdcb1453d
Admin <admin@AwesomeV-MacBook-Pro-2.local>
2020-08-10 12:39:20 +0100

    initial

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+def fib(n):
+    a = 0
+    b = 1
+    count = 0
+    while count < n:
+        c = a + b
+        print (c)
+        a = b
+        b = c
+
+        count += 1
+    return (c)
+
+n = int(input('Enter a number: '))
+fib(n)
